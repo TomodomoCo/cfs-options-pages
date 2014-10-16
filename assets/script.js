694a27ff23e88218246fbe53d630381a2a9b48c3
Chris Van Patten <chris@vanpattenmedia.com>
2014-10-16 08:39:24 -0400

    remove unnused script.js

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).on('ready', function() {
-//	jQuery('#title').prop('type', 'hidden');
-	var title = jQuery('#title').val();
-	jQuery('.wrap h2:first-of-type').text( 'Edit ' + title );
-
-	jQuery('.add-new-h2').remove();
-});
